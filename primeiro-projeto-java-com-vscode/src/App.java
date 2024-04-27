7974383840a21ee583652e8e4311e9e02a6e2397
Matheus <matheus.oliveiradesenv@proton.me>
2024-04-27 08:48:15 -0300

    integracao com vscode

---
+++ primeiro-projeto-java-com-vscode/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World , para não dar azar e Testando integração do GitHub com VisualCode");
+    }
+}
