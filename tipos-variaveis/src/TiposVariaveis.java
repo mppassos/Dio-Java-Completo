75bba3c6c1e6141c009c24c6741f7a35e22c4207
Matheus <matheus.oliveiradesenv@proton.me>
2024-04-30 07:44:08 -0300

    parametros de controle da variavel

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //Classe String representação de texto na aplicacao
+
+
+        double salarioMinimo = 2500;
+        //double salarioMinimo = 2.500 (dois virgula cinco)
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //casting
+
+        final double VALOR_DE_PI = 3.14;
+        
     }
 }
